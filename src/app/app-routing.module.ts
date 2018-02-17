d9ae0d956e5ab42b121673f702512854bacebc10
gopal <vgopalooty@gmail.com>
2018-02-17 10:33:00 +0530

    color changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,25 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AccountSummaryComponent } from './account-summary/account-summary.component';
 import { InvestmentsComponent } from './investments/investments.component';
+import { ReferralsComponent } from './referrals/referrals.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { WalletTransferComponent } from './wallet-transfer/wallet-transfer.component';
+import { WithdrawComponent } from './withdraw/withdraw.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
 { path : 'accountsummary' , component : AccountSummaryComponent },
-{ path : 'investments' , component : InvestmentsComponent}
-  
+{ path : 'investments' , component : InvestmentsComponent},
+
+ { path: 'referrals',component :ReferralsComponent },
+ {  path: 'transactions', component : TransactionsComponent },
+ {  path: 'wallettransfer', component : WalletTransferComponent },
+ {  path: 'withdraw', component : WithdrawComponent },
+ {  path: 'profile', component : ProfileComponent },
+
+//  {},
+//  {},
+
 ];
 
 @NgModule({
