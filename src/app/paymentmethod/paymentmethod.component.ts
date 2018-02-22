37e903607d2c1bc272cdf69e996d0b64d3977ceb
gopal <vgopalooty@gmail.com>
2018-02-22 22:31:41 +0530

    cc

--- src/app/paymentmethod/paymentmethod.component.ts
+++ src/app/paymentmethod/paymentmethod.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-paymentmethod',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentmethodComponent implements OnInit {
 
-  constructor() { }
+
+  /***FORM DECLARATIONS */
+  PaymentMethodForm: FormGroup;  // From Group Instance
+  amount: number;
+  paymentmethod: string;
+  /******************/
+
+
+  constructor(private fb: FormBuilder, private ds: DataService, private router: Router) {
+    this.PaymentMethodForm = fb.group({
+      'amount': '',
+      'paymentmethod': ''
+    });
+
+
+  }
 
   ngOnInit() {
+
   }
+
+  gotopayment(formdata){
+      
+console.log(formdata);
+     if(formdata.amount&&formdata.paymentmethod){
+       if(formdata.paymentmethod==='bitcoin'){
+                 
+        this.ds.NewInvestmentProcessData.investment_amount=formdata.amount;
+        this.ds.NewInvestmentProcessData.payment_method=formdata.paymentmethod;
+
+        
+       }
+     }
+  }
+
 
 }
