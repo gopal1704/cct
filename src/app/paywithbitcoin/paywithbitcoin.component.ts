52d71a2f8a863e01630f5fff33a19ffcd4ac5d32
gopal <vgopalooty@gmail.com>
2018-02-22 23:50:51 +0530

    cc

--- src/app/paywithbitcoin/paywithbitcoin.component.ts
+++ src/app/paywithbitcoin/paywithbitcoin.component.ts
@@ -2,18 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+
+declare var QRCode: any;
+
 @Component({
   selector: 'app-paywithbitcoin',
   templateUrl: './paywithbitcoin.component.html',
   styleUrls: ['./paywithbitcoin.component.css']
 })
-export class PaywithbitcoinComponent implements OnInit {
 
+
+export class PaywithbitcoinComponent implements OnInit {
+ public  InvestmentDetails : any ;
   constructor(private fb: FormBuilder, private ds : DataService, private router : Router) { }
 
   ngOnInit() {
+
+this.InvestmentDetails =this.ds.NewInvestmentProcessData;
+new QRCode(document.getElementById("qrcode"), "122YXXvEHjUTs67fXc3fFACx9PUkreXQfH");
+
+
+    var MonitorTransaction = new WebSocket('wss://ws.blockchain.info/inv');
+
   }
 
-  
+
 
 }
