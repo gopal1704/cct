3add4416bb88d4489ed4061786fc485b8210c94a
gopal <vgopalooty@gmail.com>
2018-03-11 15:01:25 +0530

    cc

--- src/app/wallettransferotp/wallettransferotp.component.ts
+++ src/app/wallettransferotp/wallettransferotp.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthenticationService } from '../authentication.service';
+import { Router } from '@angular/router';
+import { AngularFireAuth, AngularFireAuthProvider } from 'angularfire2/auth';
+import { DataService } from '../data.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-wallettransferotp',
@@ -7,7 +13,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WallettransferotpComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http :HttpClient ,private ds : DataService,private fb: FormBuilder, private afAuth: AngularFireAuth, private router: Router, private as: AuthenticationService) { }
+
+  WalletTransferOtp : FormGroup;  // From Group Instance
+  otp : any;
+  
 
   ngOnInit() {
   }
