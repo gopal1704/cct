1f389ee0338aa5d2c5ef01fda60cfc6d0ddc0b2d
gopal <vgopalooty@gmail.com>
2018-03-11 14:54:23 +0530

    cc

--- src/app/wallet-transfer/wallet-transfer.component.ts
+++ src/app/wallet-transfer/wallet-transfer.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, transition } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DataService } from '../data.service';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 
 import { AuthenticationService } from '../authentication.service';
 import { Router } from '@angular/router';
+import { validateArgCount } from '@firebase/util/dist/esm/src/validation';
 @Component({
   selector: 'app-wallet-transfer',
   templateUrl: './wallet-transfer.component.html',
@@ -14,7 +15,12 @@ export class WalletTransferComponent implements OnInit {
 
   /***FORM DECLARATIONS */
   WalletTransferForm: FormGroup;  // From Group Instance
-  
+  amountlimit:number =  1;
+
+  proceed : boolean = false;
+  amount : number = 0;
+  accountstatus : boolean  = false;
+  amountstatus : boolean = false;
   /******************/
   Accountname : any;
   constructor(private ds :DataService ,    private afs: AngularFirestore,
@@ -38,11 +44,39 @@ export class WalletTransferComponent implements OnInit {
     var name =this.afs.doc<any>(`accountsummary/${value}`).valueChanges();
       
 name.subscribe((v)=>{
+  if(v){
   this.Accountname = 'Transfer to : '+ v.name;
+ this.accountstatus = true;
+  if(this.amountstatus == true && this.accountstatus == true ){
+  this.proceed = true;
+}
+}
+  else{  this.Accountname = 'Account does not exist';
+  this.accountstatus = false;
+  this.proceed = false;
+}
 }),err=>{
   this.Accountname = 'Account does not exist';
 }
   }
+
+
+  ///////
+
+  onAmountChange(value : number){
+if(value >= this.amountlimit){
+this.amountstatus = true;
+if(this.amountstatus == true && this.accountstatus == true ){
+  this.proceed = true;
+}
+}
+else{
+  this.amountstatus = false;
+  this.proceed = false;
+
+}
+  }
+  /////
   
   transfer(formdata){
 console.log(formdata);
